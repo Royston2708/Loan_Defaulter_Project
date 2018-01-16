c9c30cd3f4a29eae17dfbb391356070a7cdd7af7
royston <royston.soares@gmail.com>
2018-01-16 14:57:57 +0530

    strings and bubble sort

---
+++ week_1/Introduction Course Practice.py
@@ -0,0 +1,29 @@
+# bubble sort
+
+def sort(list):
+    for num in range(len(list)-1, 0,-1):
+        #num is an iterator like i which has the starting value of the range,
+        # so that it runs all the passes needed to sort out the entire list
+        for i in range(num):
+            if list[i]>list[i+1]:
+                temp= list[i]
+                list[i]=list[i+1]
+                list[i+1]=temp
+
+list = [32,4,67,90,12,43,23,542,42,665,23,54,765,86.9,56,3,1,75,43]
+sort(list)
+print(list)
+print("\n")
+
+#Strings and manipulation of strings
+fruit = "banana"
+length = len(fruit)
+print("The length is equal to", length)
+welcome = "greetings on your introduction to this lecture on python. you and me makes we"
+
+x= welcome.find('on')
+print(x)
+y= welcome.find('.')
+print(y)
+final = welcome[x:y]
+print(final)
