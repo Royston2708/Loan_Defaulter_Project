ef7c589eef9cd81c284e7abdfc2cf85cd12dc60e
roysron <royston.soares@gmail.com>
2018-01-24 14:27:48 +0530

    numpy

---
+++ untitled1/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib as mtlib
+
+height = [1.72, 1.69, 1.85, 2.01, 1.79]
+np_height = np.array(height)
+print("heights are", np_height)
+weight= [65.4, 59.2, 63.6, 88.4, 68.7]
+np_weight= np.array(weight)
+print("weights are", np_weight, "\n")
+bmi = np_weight/np_height**2
+print("bmi data is", bmi)
