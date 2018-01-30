db792293eb22e21ba5377526acec3b5a94484204
roysron <royston.soares@gmail.com>
2018-01-30 11:12:45 +0530

    hi

--- untitled1/histograms.py
+++ untitled1/histograms.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-values = np.random.normal(25,3,1000)
+values = np.random.exponential(100,1000)
 plt.hist(values,bins=10)
+plt.xlabel("Values")
+plt.ylabel("Magnitude")
+plt.title("Test Plot of Random Numbers")
+plt.yticks([0,50,100,150,200,250])
+
 plt.show()
 plt.clf()
 
@@ -10,3 +15,7 @@ values_2 = values * 0.65
 plt.hist(values_2,bins=20)
 plt.show()
 plt.clf()
+
+
+# Week 3 continued
+plt.scatter(values)
