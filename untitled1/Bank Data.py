2068918e88dafba8e9a69e8e32bcf355c90b3c53
roysron <royston.soares@gmail.com>
2018-02-05 13:14:58 +0530

    bs-2

--- untitled1/Bank Data.py
+++ untitled1/Bank Data.py
@@ -5,7 +5,9 @@ import requests
 import seaborn as sns
 from collections import Counter
 
-df_data = pd.read_csv("/home/user/Downloads/Data Sources/bank-full.csv", sep= ";", nrows = 1000)
+
+sns.set()
+df_data = pd.read_csv("/home/user/Downloads/Data Sources/bank-full.csv", sep= ";")
 print(df_data.head())
 bins = df_data["job"].unique()
 
@@ -13,7 +15,16 @@ print("Hello")
 array = [item for item in df_data["job"]]
 letter_counts = Counter(array)
 hist_df = pd.DataFrame.from_dict(letter_counts, orient = 'index')
-hist_df.plot(kind ='bar', figsize = (7, 7), fontsize = 7)
+hist_df.plot(kind ='bar', figsize = (5, 5), fontsize = 7)
 plt.show()
+married_and_house = df_data[(df_data["marital"] == "married") & (df_data["housing"] == "yes")]
+print(married_and_house)
+
+age = [int(item) for item in married_and_house["age"]]
+print(age)
+
+plt.hist(age)
+plt.show()
+
 
 # Well done
