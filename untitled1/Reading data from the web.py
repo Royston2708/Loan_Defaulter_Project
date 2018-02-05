dc7bd5a51b38a7475fbff0e925cdcd8264b86603
roysron <royston.soares@gmail.com>
2018-02-05 12:59:15 +0530

    hi

--- untitled1/Reading data from the web.py
+++ untitled1/Reading data from the web.py
@@ -1,5 +1,7 @@
-import pandas as pd
+
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import requests
 
 url = "https://www.wikipedia.org/"
@@ -8,4 +10,14 @@ text = r.text
 
 print (text)
 
-# TWITTER API PROJECT
+
+df_election = pd.read_csv("/home/user/Downloads/Data Sources/US Election full data.csv")
+print(df_election)
+
+plt.hist(df_election["dem_share"], bins= 20)
+plt.grid()
+plt.xlabel("Instances")
+plt.ylabel("Percentage")
+plt.show()
+
+
