cf90ec8dd34909870f0969530fae88dd7db21d35
roysron <royston.soares@gmail.com>
2018-02-19 13:30:29 +0530

    hi

--- Week 4/test.py
+++ Week 4/test.py
@@ -5,34 +5,70 @@ import sklearn
 from sklearn.neighbors import KNeighborsClassifier
 import seaborn as sns
 
-# The Objective of this code is to build a K-Nearest Neighbours Classifier for the Bank Data that we have
-file1 = pd.read_csv("/home/user/Downloads/portugese bank/bank-full-encoded.csv", sep=";" ,parse_dates= True)
-print(file1.shape)
+def main():
+    global prediction, test_data ,train_Data
+    # The Objective of this code is to build a K-Nearest Neighbours Classifier for the Bank Data that we have
+    file1 = pd.read_csv("/home/user/Downloads/portugese bank/bank-full-encoded.csv", sep=";" ,parse_dates= True)
+    print(file1.shape)
 
-# Setting Plot Style To Seaborn
-sns.set()
+    # Setting Plot Style To Seaborn
+    sns.set()
 
-#Creating a Countplot of the count of people who are middle aged vs whether or not they were approved for a loan
-sns.countplot("age_mid", hue = "y", data = file1, palette = "RdBu")
-plt.xticks([0,1], ["NOT MIDDLE AGED","MIDDLE AGED"])
-plt.title("AGE")
-plt.show()
+    #Creating a Countplot of the count of people who are middle aged vs whether or not they were approved for a loan
+    sns.countplot("age_mid", hue = "y", data = file1, palette = "RdBu")
+    plt.xticks([0,1], ["NOT MIDDLE AGED","MIDDLE AGED"])
+    plt.title("AGE")
+    plt.show()
 
-# Determining the correlation coefficient
-print(file1["age_mid"].values)
-print("\nThe Correlation Coefficient between middle age and approval for a loan is = ")
-print(np.corrcoef(file1["age_mid"].values, file1["y"].values))
+    # Splitting Data into Training set and test Set
+    train_Data = file1.iloc[:32000,:]
+    test_data =file1.iloc[32000:,:]
 
-# Building a KNN Predictor model for the data
-y = file1["y"].values
-x = file1.drop("y", axis = 1).values
+    # Building a KNN Predictor model for the data
+    y = train_Data["y"].values
+    x = train_Data.drop("y", axis = 1).values
 
-knn = KNeighborsClassifier(n_neighbors= 10)
-knn.fit(x,y)
+    knn = KNeighborsClassifier(n_neighbors= 10)
+    knn.fit(x,y)
 
-np.random.seed(50)
-x_new = [np.random.randint(0,2,26)]
-print(x_new)
+    #Predicting using our aldready labelled data set
 
-prediction = knn.predict(x_new)
-print("The label of the new value is = ",prediction)
+    prediction = knn.predict(test_data.drop("y", axis = 1))
+    count_0 = 0
+    count_1 = 0
+    for i in prediction:
+        if i == 0 :
+            count_0 += 1
+        else:
+            count_1 += 1
+        print(i)
+    return count_0, count_1
+
+zero , one = main()
+print("count of zeroes is =", zero,"\n count of ones is =", one)
+
+# Accuracy of the model
+
+wrong_prediction = 0
+correct_prediction = 0
+i = 0
+for element in prediction:
+    if element != test_data["y"].iloc[i]:
+        wrong_prediction +=1
+    else:
+        correct_prediction += 1
+    i+= 1
+
+print("Number of incorrect predictions =", wrong_prediction,"\nNumber of correct predictions =", correct_prediction)
+print("Accuracy of model =", (correct_prediction/(wrong_prediction+correct_prediction))*100, "%")
+
+
+
+# #Running KNN with random Data set
+# np.random.seed(50)
+# x_new = [np.random.randint(0,2,26)]
+# print("\n\n", x_new)
+#
+# prediction_random = knn.predict(x_new)
+# print("The label of the new value is = ",prediction_random)
+#
