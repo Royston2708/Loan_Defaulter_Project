cac14f9f62da385948e487fd51ebd24802477ff9
roysron <royston.soares@gmail.com>
2018-02-20 10:50:32 +0530

    hi

--- Week 4/test.py
+++ Week 4/test.py
@@ -6,47 +6,48 @@ from sklearn.neighbors import KNeighborsClassifier
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
 
-def main():
-    global prediction, test_data ,train_Data, file1
-    # The Objective of this code is to build a K-Nearest Neighbours Classifier for the Bank Data that we have
-    file1 = pd.read_csv("/home/user/Downloads/portugese bank/bank-full-encoded.csv", sep=";" ,parse_dates= True)
-    print(file1.shape)
 
-    # Setting Plot Style To Seaborn
-    sns.set()
+# The Objective of this code is to build a K-Nearest Neighbours Classifier for the Bank Data that we have
+file1 = pd.read_csv("/home/user/Downloads/portugese bank/bank-full-encoded.csv", sep=";" ,parse_dates= True)
+print(file1.shape)
 
-    #Creating a Countplot of the count of people who are middle aged vs whether or not they were approved for a loan
-    sns.countplot("age_mid", hue = "y", data = file1, palette = "RdBu")
-    plt.xticks([0,1], ["NOT MIDDLE AGED","MIDDLE AGED"])
-    plt.title("AGE")
-    plt.show()
+# Setting Plot Style To Seaborn
+sns.set()
 
-    # Splitting Data into Training set and test Set
-    train_Data = file1.iloc[:32000,:]
-    test_data =file1.iloc[32000:,:]
+#Creating a Countplot of the count of people who are middle aged vs whether or not they were approved for a loan
+sns.countplot("age_mid", hue = "y", data = file1, palette = "RdBu")
+plt.xticks([0,1], ["NOT MIDDLE AGED","MIDDLE AGED"])
+plt.title("AGE")
+plt.show()
 
-    # Building a KNN Predictor model for the data
-    y = train_Data["y"].values
-    x = train_Data.drop("y", axis = 1).values
+# Splitting Data into Training set and test Set
+train_Data = file1.iloc[:32000,:]
+test_data =file1.iloc[32000:,:]
 
-    knn = KNeighborsClassifier(n_neighbors= 10)
-    knn.fit(x,y)
+# Building a KNN Predictor model for the data
+y = train_Data["y"].values
+x = train_Data.drop("y", axis = 1).values
 
-    #Predicting using our aldready labelled data set
+knn = KNeighborsClassifier(n_neighbors= 10)
+knn.fit(x,y)
 
-    prediction = knn.predict(test_data.drop("y", axis = 1))
-    count_0 = 0
-    count_1 = 0
-    for i in prediction:
-        if i == 0 :
-            count_0 += 1
-        else:
-            count_1 += 1
-        print(i)
-    return count_0, count_1
+#Predicting using our aldready labelled data set
 
-zero , one = main()
+prediction = knn.predict(test_data.drop("y", axis = 1))
+count_0 = 0
+count_1 = 0
+for i in prediction:
+    if i == 0 :
+        count_0 += 1
+    else:
+        count_1 += 1
+    print(i)
+
+zero = count_0
+one = count_1
+
 print("count of zeroes is =", zero,"\n count of ones is =", one)
 
 # Accuracy of the model
@@ -102,3 +103,14 @@ for element in y_prediction:
 
 print("Number of incorrect predictions =", wrong_prediction_reg,"\nNumber of correct predictions =", correct_prediction_reg)
 print("Accuracy of model =", (correct_prediction_reg/(wrong_prediction_reg+correct_prediction_reg))*100, "%")
+
+
+# Building Confusion Matrix for the 2 models
+
+print("\nThe Confusion Matrix for the KNN Model is as follows: \n", confusion_matrix(test_data["y"].values,prediction))
+
+print("\nThe Classification Report for the KNN Model is as Follows:\n", classification_report(test_data["y"].values, prediction))
+
+print("\nThe Confusion Matrix for the Log-Reg model is as follows:\n", confusion_matrix(y_test,y_prediction))
+
+print("\nThe Classification Report for the Log-Reg model is as follows:\n", classification_report(y_test, y_prediction))
