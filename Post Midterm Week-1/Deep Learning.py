0365743b4ea930d9672692c0f597dc48c8d94de5
roysron <royston.soares@gmail.com>
2018-04-02 15:34:37 +0530

    hi

--- Post Midterm Week-1/Deep Learning.py
+++ Post Midterm Week-1/Deep Learning.py
@@ -3,4 +3,11 @@ import numpy as np
 import tensorflow as tf
 
 print("hello royston welcome to your first deep learning module")
+x = tf.constant([5])
+y = tf.constant([6])
 
+result = tf.multiply(x,y)
+
+print(result)
+sess = tf.Session()
+print("\n", sess.run(result))
