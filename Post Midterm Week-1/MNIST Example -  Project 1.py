98365443587963dbe692e1967ac63ba1dfc851a4
roysron <royston.soares@gmail.com>
2018-04-05 11:17:16 +0530

    hi

--- Post Midterm Week-1/MNIST Example -  Project 1.py
+++ Post Midterm Week-1/MNIST Example -  Project 1.py
@@ -16,26 +16,26 @@ y = tf.placeholder("float")
 
 def neural_network_model(data):
     hidden_1_layer  = {"weights": tf.Variable(tf.random_normal([784,n_nodes_hl1])),
-                       "biases": tf.Variable(tf.random_normal(n_nodes_hl1))}
+                       "biases": tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      "biases": tf.Variable(tf.random_normal(n_nodes_hl2))}
+                      "biases": tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
     hidden_3_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      "biases": tf.Variable(tf.random_normal(n_nodes_hl3))}
+                      "biases": tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
     output_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                      "biases": tf.Variable(tf.random_normal(n_classes))}
+                      "biases": tf.Variable(tf.random_normal([n_classes]))}
 
 # The network format is ((input* weights) + biases)
 
-    l1 = tf.add(tf.matmul(data,hidden_1_layer["weights"]),hidden_1_layer["biases"])
+    l1 = tf.add(tf.matmul(data,hidden_1_layer["weights"]), hidden_1_layer["biases"])
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(data,hidden_2_layer["weights"]),hidden_2_layer["biases"])
+    l2 = tf.add(tf.matmul(data,hidden_2_layer["weights"]), hidden_2_layer["biases"])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(data, hidden_3_layer["weights"]),hidden_3_layer["biases"])
+    l3 = tf.add(tf.matmul(data, hidden_3_layer["weights"]), hidden_3_layer["biases"])
     l3 = tf.nn.relu(l3)
 
     output = tf.matmul(l3,output_layer["weights"]) + output_layer["biases"]
@@ -52,7 +52,7 @@ def train_neural_net(x):
     with tf.Session as sess:
         sess.run(tf.initialize_all_variables())
 
-        for epoch in hm_epochs:
+        for epoch in range(hm_epochs):
             epoch_cost = 0
             for _ in range(int(mnist.train.num_examples/batch_size)):
 
@@ -61,3 +61,6 @@ def train_neural_net(x):
                 epoch_cost += c
 
             print("Epoch", epoch , "completed out of:", hm_epochs , "Cost:", epoch_cost)
+
+        correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct,"float"))
